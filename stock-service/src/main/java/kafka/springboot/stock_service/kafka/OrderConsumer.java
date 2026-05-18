1bcafbe93d7f39cb99a0e18a61d36e3c91f172f5
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-05-17 22:40:19 -0500

    Feat: Created OrderConsumer class
    Created an OrderConsumer class that ingested messages from the topic
    from the Kafka broker and printed out a confirmation message.

---
+++ stock-service/src/main/java/kafka/springboot/stock_service/kafka/OrderConsumer.java
@@ -0,0 +1,29 @@
+package kafka.springboot.stock_service.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import kafka.springboot.base_domains.dto.OrderEvent;
+
+/*
+ *
+ * The OrderConsumer class simply consumes messages from a partition in a topic 
+ * from the Kafka broker. The @KafkaListener annotation turns the consume function
+ * into a bean method as a listener for a listener container. It turns the function
+ * into a MessagingMessageLIstenerAdapter. The @Service annotation is just a generic
+ * bean configuration.
+ *
+ */
+@Service
+public class OrderConsumer {
+  // Log the event that we consume from the kafka topic
+  private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+  @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+  public void consume(OrderEvent event) {
+    LOGGER.info(String.format("Order event received in stock service: %s", event.toString()));
+
+    // Saving the order event into the database (to be implemented later)
+  }
+}
